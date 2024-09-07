7812781b7ae02aebfde720538d34e93dd2bac25e
saaaalmon <salmonsaidolimov0@gmail.com>
2024-09-07 10:54:59 +0300

    Практика 1

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let ram = prompt('Введите барашка','Неизвестная барашка');
+let breed = prompt('Введите породу','Неизвестная порода');
+let number = prompt('Введите количество','Неизвестное количество');
+let isTrue = confirm(`у вас точно ${ram} ${breed} ${number}`);
+alert (isTrue)
+
